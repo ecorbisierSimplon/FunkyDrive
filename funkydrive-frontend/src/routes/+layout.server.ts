9025dfb3bea36cee97614ed5eb8659dacaaaa9dc
ecorbisierSimplon <ecorbisier.simplon@gmail.com>
2024-03-11 09:45:14 +0100

    Eric/Bug fix .env

--- funkydrive-frontend/src/routes/+layout.server.ts
+++ funkydrive-frontend/src/routes/+layout.server.ts
@@ -1,7 +1,7 @@
 // since there's no dynamic data here, we can prerender
 import type { PageServerLoad } from './$types';
 import { getWritable } from '$lib/packages/getWritable';
-import { API_CLIENT_URL } from '$lib/packages/variables';
+import { API_CLIENT_URL } from '$lib/server/variables';
 
 // it so that it gets served as a static asset in production
 
