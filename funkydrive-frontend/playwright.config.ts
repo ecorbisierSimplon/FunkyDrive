13139965e3c15470c4221ce9a83c867ffcaccb84
ecorbisierSimplon <ecorbisier.simplon@gmail.com>
2024-03-18 15:45:11 +0100

    Eric/Add test for login if input blanck

--- funkydrive-frontend/playwright.config.ts
+++ funkydrive-frontend/playwright.config.ts
@@ -5,9 +5,12 @@ const config: PlaywrightTestConfig = {
 	// 	command: 'npm run build && npm run preview',
 	// 	port: 4173
 	// },
+	use: {
+		baseURL: 'http://localhost:8000/',
+	  },
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
-	timeout: 2_000,
+	timeout: ((0 * 60 ) + (25)) * 1000,
 };
 
 export default config;
